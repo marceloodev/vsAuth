b64d54d12f3169da29beba62dff2bc0e7c16487b
Vieira's Store <98975919+VieirasStore@users.noreply.github.com>
2022-11-09 12:49:59 -0300

    Update vs-server.lua

--- vs-server.lua
+++ vs-server.lua
@@ -10,13 +10,13 @@ end
 -------------------------------------------
 -- CONFIGURAÇÕES DA AUTH ⬇
 -------------------------------------------
-local AuthIsOK = "XXXXX" -- Coloque sua webhook substituindo os XXXXX
-local AuthIsNotOK = "XXXXX" -- Coloque sua webhook substituindo os XXXXX
-local AuthIsTimeOut = "XXXXX" -- Coloque sua webhook substituindo os XXXXX
+local AuthorizationOK = "XXXXX" -- Coloque sua webhook substituindo os XXXXX
+local AuthorizationIsNotOK = "XXXXX" -- Coloque sua webhook substituindo os XXXXX
+local AuthorizationIsTimeOut = "XXXXX" -- Coloque sua webhook substituindo os XXXXX
 
-local authorizationIp = "0.0.0.0" -- Substitua 0.0.0.0 com o IP de quem poderá autenticar esse script. (Site para pegar o IP: https://meuip.com.br)
+local AuthorizationIp = "0.0.0.0" -- Substitua 0.0.0.0 com o IP de quem poderá autenticar esse script. (Site para pegar o IP: https://meuip.com.br)
 
-local AuthScript = false -- Deixe em false para a autenticação funcionar
+local AuthorizationScript = false -- Deixe em false para a autenticação funcionar
 -------------------------------------------
 -- CONFIGURAÇÕES DA AUTH ^
 -------------------------------------------
@@ -26,30 +26,30 @@ if debug.getinfo( PerformHttpRequest ).short_src:find("citizen:/scripting/lua/sc
 PerformHttpRequest("http://api.ipify.org/", function(err, data)
     if err >= 200 and err <= 299 then
         local receivedIp = data
-        if authorizationIp == receivedIp then
+        if AuthorizationIp == receivedIp then
             AuthScript = true
             Wait(1000)
             print("^2[resource_name] Autenticado - Qualquer duvida -> Name#XXXX") -- Substitua Name#XXXX Pelo seu discord EX: EuNoah#8056
-            SendWebhookMessage(AuthIsOK,"```prolog\n[O IP]: "..receivedIp.."\n[AUTENTICOU]: O Script [resource_name]" ..os.date("\n[Data]: %d/%m/%Y [Hora]: %H:%M:%S").." \r```")
+            SendWebhookMessage(AuthorizationIsOK,"```prolog\n[O IP]: "..receivedIp.."\n[AUTENTICOU]: O Script [resource_name]" ..os.date("\n[Data]: %d/%m/%Y [Hora]: %H:%M:%S").." \r```")
         else
-            AuthScript = false
+            AuthorizationScript = false
             Wait(1000)
             print("^8[resource_name] Não autenticado - Qualquer duvida -> Name#XXXX") -- Substitua Name#XXXX Pelo seu discord EX: EuNoah#8056
-            SendWebhookMessage(AuthIsNotOK,"```prolog\n[O IP]: "..receivedIp.."\n[NÃO AUTENTICOU]: O Script [resource_name] utilizando a licença do IP: "..authorizationIp.." " ..os.date("\n[Data]: %d/%m/%Y [Hora]: %H:%M:%S").." \r```")
+            SendWebhookMessage(AuthorizationIsNotOK,"```prolog\n[O IP]: "..receivedIp.."\n[NÃO AUTENTICOU]: O Script [resource_name] utilizando a licença do IP: "..authorizationIp.." " ..os.date("\n[Data]: %d/%m/%Y [Hora]: %H:%M:%S").." \r```")
             Wait(10000)
             os.exit()
         end
     else
-        AuthScript = false
+        AuthorizationScript = false
         Wait(1000)
         print("^3[resource_name] Não autenticado - Falha na autenticação - Avise o -> Name#XXXX") -- Substitua Name#XXXX Pelo seu discord EX: EuNoah#8056
-        SendWebhookMessage(AuthIsTimeOut,"```prolog\n[O IP]: N/A \n[NÃO AUTENTICOU]: O Script [resource_name] Pois não conseguimos pegar o IP dele." ..os.date("\n[Data]: %d/%m/%Y [Hora]: %H:%M:%S").." \r```")
+        SendWebhookMessage(AuthorizationIsTimeOut,"```prolog\n[O IP]: N/A \n[NÃO AUTENTICOU]: O Script [resource_name] Pois não conseguimos pegar o IP dele." ..os.date("\n[Data]: %d/%m/%Y [Hora]: %H:%M:%S").." \r```")
         Wait(10000)
         os.exit()
     end
 end)
 else
-    AuthScript = false
+    AuthorizationScript = false
     print("^3[resource_name] Nao autenticado - Codigo de burlamento de auth encontrado. - Qualquer duvida -> Name#XXXX") -- Substitua Name#XXXX Pelo seu discord EX: EuNoah#8056
     os.exit()
 end
